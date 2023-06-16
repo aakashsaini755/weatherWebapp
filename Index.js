769d027c5d754938610b600b7f01c3a8a217ce2f
Aakash Saini <aakashsaini755@gmail.com>
2023-06-16 17:21:00 +0530

    firstcommit

---
+++ Index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+  return temperature;
+};
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=rohtak&appid=5cb9be259f672ab72fb56557ec99f462"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arr = [objdata];
+        // console.log(arr[0].main.temp);
+        const realTimeData = arr.map((val) => replaceVal(homeFile, val)).join("");
+        res.write(realTimeData);
+        // console.log(realTimeData)
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+});
+
+server.listen(5000, "127.0.0.1");
